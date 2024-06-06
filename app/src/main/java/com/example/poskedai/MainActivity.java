eec4b127d58fd6563474cad9225c23782f56262f
adi3947 <adi3947@gmail.com>
2024-06-06 21:20:50 +0700

    second commit

--- app/src/main/java/com/example/poskedai/MainActivity.java
+++ app/src/main/java/com/example/poskedai/MainActivity.java
@@ -1,24 +1,62 @@
 package com.example.poskedai;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    FrameLayout content;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNavigationView = findViewById(R.id.bottom_navbar);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.homepage) {
+                    openFragment(new MainFragment());
+                    return true;
+                }
+                if (item.getItemId() == R.id.menu) {
+                    openFragment(new MenuFragment());
+                    return true;
+                }
+                if (item.getItemId() == R.id.cart) {
+                    openFragment(new CartFragment());
+                    return true;
+                }
+                if (item.getItemId() == R.id.account) {
+                    openFragment(new AccountFragment());
+                    return true;
+                }
+                return false;
+            }
         });
+    }
+
+
+    public boolean openFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.content, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }

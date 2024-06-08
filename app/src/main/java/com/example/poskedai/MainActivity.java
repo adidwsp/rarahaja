719f0031a3e37f50901b7fe2a6cb987961f1e942
adi3947 <adi3947@gmail.com>
2024-06-08 18:36:01 +0700

    second commit

--- app/src/main/java/com/example/poskedai/MainActivity.java
+++ app/src/main/java/com/example/poskedai/MainActivity.java
@@ -4,19 +4,14 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    FrameLayout content;
     BottomNavigationView bottomNavigationView;
 
     @Override
@@ -47,10 +42,10 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+        openFragment(new MainFragment());
     }
 
-
-    public boolean openFragment(Fragment fragment) {
+    Boolean openFragment(Fragment fragment) {
         if (fragment != null) {
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.content, fragment)
